80d64c17677f00c83ebb897f69127ab4c0952712
Majd <clvansa@gmail.com>
2020-01-28 15:57:06 +0100

    majd

---
+++ main.js
@@ -0,0 +1,13 @@
+const menu = document.querySelector('.menu').children[0];
+const myList = document.querySelector('.myList')
+
+
+menu.addEventListener('click',()=>{
+    if(myList.style.display === 'none'){
+        myList.style.display = 'block'
+    }else{
+
+        myList.style.display = 'none';
+    }
+    
+})
